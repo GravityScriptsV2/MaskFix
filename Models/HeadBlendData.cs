5358999e322b1f621cb84f710aa7c599e2a0f706
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-06-27 23:28:26 -0700

    Add Model

---
+++ Models/HeadBlendData.cs
@@ -0,0 +1,12 @@
+﻿public class HeadBlendData
+{
+    public int ShapeFirst { get; set; }
+    public int ShapeSecond { get; set; }
+    public int ShapeThird { get; set; }
+    public int SkinFirst { get; set; }
+    public int SkinSecond { get; set; }
+    public int SkinThird { get; set; }
+    public float ShapeMix { get; set; }
+    public float SkinMix { get; set; }
+    public float ThirdMix { get; set; }
+}
