59cda7c22448f5f513ecde654026fb9d5519c64a
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-06-28 00:30:29 -0700

    Fix HeadBlendData

--- Models/HeadBlendData.cs
+++ Models/HeadBlendData.cs
@@ -2,47 +2,34 @@
 
 namespace MaskFix.Models 
 {
-    /// <summary>
-    /// Represents the native structure layout for head blend data.
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    public struct HeadBlendDataStruct
+    [StructLayout(LayoutKind.Explicit, Size = 80)]
+    public struct HeadBlendData
     {
-        public int shapeFirst;
-        public int padding1;
-        public int shapeSecond;
-        public int padding2;
-        public int shapeThird;
-        public int padding3;
-        public int skinFirst;
-        public int padding4;
-        public int skinSecond;
-        public int padding5;
-        public int skinThird;
-        public int padding6;
-        public float shapeMix;
-        public int padding7;
-        public float skinMix;
-        public int padding8;
-        public float thirdMix;
-        public int padding9;
-        [MarshalAs(UnmanagedType.I1)]
-        public bool isParent;
-    }
+        [FieldOffset(0)]
+        public int ShapeFirst;
 
-    /// <summary>
-    /// Represents the high-level abstraction of head blend data.
-    /// </summary>
-    public class HeadBlendData
-    {
-        public int ShapeFirst { get; set; }
-        public int ShapeSecond { get; set; }
-        public int ShapeThird { get; set; }
-        public int SkinFirst { get; set; }
-        public int SkinSecond { get; set; }
-        public int SkinThird { get; set; }
-        public float ShapeMix { get; set; }
-        public float SkinMix { get; set; }
-        public float ThirdMix { get; set; }
+        [FieldOffset(8)]
+        public int ShapeSecond;
+
+        [FieldOffset(16)]
+        public int ShapeThird;
+
+        [FieldOffset(24)]
+        public int SkinFirst;
+
+        [FieldOffset(32)]
+        public int SkinSecond;
+
+        [FieldOffset(40)]
+        public int SkinThird;
+
+        [FieldOffset(48)]
+        public float ShapeMix;
+
+        [FieldOffset(56)]
+        public float SkinMix;
+
+        [FieldOffset(64)]
+        public float ThirdMix;
     }
 }
